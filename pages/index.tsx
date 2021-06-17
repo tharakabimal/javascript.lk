77fa398e99d1ba599afb5b9589f7260501f6b7d9
tharakabimal <tharaka.bimal@gmail.com>
2021-06-17 12:11:13 +0530

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home = () => (
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
             <h1 className="text-6xl font-bold">
                 Welcome to{' '}
                 <a className="text-blue-600" href="https://nextjs.org">
