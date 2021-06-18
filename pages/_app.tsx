ad87a28a2f602060e2a354378c9d93850c7be94f
tharakabimal <tharaka.bimal@gmail.com>
2021-06-18 09:58:24 +0530

    ts linting and husky config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
 
